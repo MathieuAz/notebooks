083e8ab3cc78ae38c1cc4f95f92bb8a7cb91b8a8
Florian Pellet <fpellet@ensc.fr>
2020-08-04 15:28:24 +0200

    fix typo on relative links hrefs

--- src/main.ts
+++ src/main.ts
@@ -67,7 +67,7 @@ async function list() {
     const html = tree
         .filter(({type}) => type === 'blob')
         .map(({path}) => `
-        <li><a href="/?file=${path}">${path}</a></li>
+        <li><a href="?file=${path}">${path}</a></li>
         `)
         .join('');
     document.body.innerHTML += html;
